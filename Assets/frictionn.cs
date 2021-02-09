9b35ac7fd7e085f2647aa4af2f4442b8911e0c30
Evelyn Tingqi Fan <tfan1@inside.artcenter.edu>
2021-02-09 11:07:33 +0800

    9

---
+++ Assets/frictionn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frictionn : MonoBehaviour
+{
+    public float Drag;
+
+    public string NameOfDragger;
+
+    private Rigidbody2D thisRigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigidbody2D = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay2D (Collider2D collision)
+    {
+        if (collision. gameObject. name == NameOfDragger)
+        {
+            Vector3 velocityOfDragger = collision. gameObject.GetComponent<Rigidbody2D>(). velocity;
+            thisRigidbody2D.AddForce (Drag * velocityOfDragger);
+        }
+
+    }
+
+
+}
