25a947e5cfa791410b7181b27625bbbe42ae08b5
Evelyn Tingqi Fan <tfan1@inside.artcenter.edu>
2021-02-09 10:16:27 +0800

    8

---
+++ Assets/vertex.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class vertex : MonoBehaviour
+{
+    public Transform[] CornerObjects;
+    private SpriteShapeController spriteShapeController;
+    void Start()
+    {
+        spriteShapeController = GetComponent<SpriteShapeController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int pointID = 0 ; pointID < CornerObjects. Length; pointID++)
+        {
+            spriteShapeController.spline.SetPosition(pointID, CornerObjects [pointID]. localPosition);
+        }
+
+    }
+}
