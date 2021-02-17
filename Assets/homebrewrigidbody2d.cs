21773d43a1fe16c1d48c86e91c3980c240646246
Evelyn Tingqi Fan <tfan1@inside.artcenter.edu>
2021-02-17 16:48:59 +0800

    4.1

---
+++ Assets/homebrewrigidbody2d.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homebrewrigidbody2d : MonoBehaviour
+{
+    public Vector3 Velocity, Acceleration,TotalForce,Gravity;
+
+    public Vector3 gravity;
+
+    public float Mass;
+
+    private void FixedUpdate()
+    {
+        TotalForce += gravity;
+        transform.position += Velocity * Time.fixedDeltaTime;
+        Velocity += Acceleration * Time.fixedDeltaTime;
+        Acceleration = TotalForce / Mass;
+        TotalForce = Vector3.zero;
+    }
+
+
+        
+    
+}
---
+++ Assets/selfdest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfdest : MonoBehaviour
+{
+    public float Lifespan;
+
+    private float TimeOfDeath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeOfDeath = Time.time + Lifespan;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > TimeOfDeath)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
